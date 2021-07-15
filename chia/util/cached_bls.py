ef59d1b9ff9f87d7fc6349e62044e18beb0f86a7
Rostislav <rostislav@users.noreply.github.com>
2021-07-15 19:55:04 +0300

    Fix issues in cached_bls found in code review

--- chia/util/cached_bls.py
+++ chia/util/cached_bls.py
@@ -1,8 +1,8 @@
 import functools
-import hashlib
-from typing import List
+from typing import List, Optional
 
 from blspy import AugSchemeMPL, G1Element, G2Element, GTElement
+from chia.util.hash import std_hash
 from chia.util.lru_cache import LRUCache
 
 
@@ -10,9 +10,9 @@ def get_pairings(cache: LRUCache, pks: List[G1Element], msgs: List[bytes], force
     pairings: List[GTElement] = []
     missing_count: int = 0
     for pk, msg in zip(pks, msgs):
-        aug_msg = bytes(pk) + msg
-        h = hashlib.sha256(aug_msg).digest()
-        pairing = cache.get(h)
+        aug_msg: bytes = bytes(pk) + msg
+        h: bytes = bytes(std_hash(aug_msg))
+        pairing: Optional[GTElement] = cache.get(h)
         if pairing is None:
             missing_count += 1
             # Heuristic to avoid more expensive sig validation with pairing
@@ -22,13 +22,13 @@ def get_pairings(cache: LRUCache, pks: List[G1Element], msgs: List[bytes], force
                 return []
         pairings.append(pairing)
 
-    for i in range(len(pairings)):
-        if pairings[i] is None:
+    for i, pairing in enumerate(pairings):
+        if pairing is None:
             aug_msg = bytes(pks[i]) + msgs[i]
-            aug_hash = AugSchemeMPL.g2_from_message(aug_msg)
+            aug_hash: G2Element = AugSchemeMPL.g2_from_message(aug_msg)
             pairing = pks[i].pair(aug_hash)
 
-            h = hashlib.sha256(aug_msg).digest()
+            h = bytes(std_hash(aug_msg))
             cache.put(h, pairing)
             pairings[i] = pairing
 
@@ -41,8 +41,8 @@ local_cache = LRUCache(10000)
 def aggregate_verify(pks: List[G1Element], msgs: List[bytes], sig: G2Element, force: bool = False):
     if len(pks) == 0 and sig == G2Element():
         return True
-    pairings = get_pairings(local_cache, pks, msgs, force)
-    if not pairings:
+    pairings: List[GTElement] = get_pairings(local_cache, pks, msgs, force)
+    if len(pairings) == 0:
         return AugSchemeMPL.aggregate_verify(pks, msgs, sig)
 
     pairings_prod: GTElement = functools.reduce(GTElement.__mul__, pairings)
